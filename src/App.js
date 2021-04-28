848de89461e34ef01b198d7f66e49bf1d4741894
SURYANARAYAN RATH <45998114+surya-trv-13@users.noreply.github.com>
2021-04-28 12:07:59 +0530

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./styles.css";
+import { ClassComponent } from "./component/ClassComponent";
 
+// const App1 = () => {
+//     App();
+// }
+// Parent Component
 export default function App() {
+  const object = ["x", "y"];
   return (
     <div className="App">
       <h1>React KT</h1>
       <h2>Class Component</h2>
+      <ClassComponent data="Data 1" data2="Anotehr" />
+      <ClassComponent data="Data 2" />
+      <ClassComponent data={object} />
+      <ClassComponent />
     </div>
   );
 }
---
+++ src/component/ClassComponent.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+// let v3 = "LET"
+// const variable1 = "Constatn";
+
+// Child Component
+class ClassComponent extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  // obj = { RAM: "rAM", sHYAM: "sHAM" };
+
+  check = () => {
+    const variable1 = "Contant";
+    let v2 = "Let";
+    var v3 = "Var";
+    return v3;
+  };
+
+  // check2 = () => {
+  //   console.log(variable1);
+  //   console.log(v3);
+  // }
+
+  render() {
+    let { data, data2 } = this.props;
+    // let { RAM, sHAM } = this.obj; //' Object Destructuring
+    // RAM = "Surya";
+    data = "Surya";
+    // console.log(RAM);
+    const v3 = this.check();
+    return (
+      <h1>
+        This is Class Component {data} {data2}
+      </h1>
+    );
+  }
+}
+
+export { ClassComponent };
+//
