4b64d457dfcc9c6264365a1075d4e4bb2448351a
SURYANARAYAN RATH <45998114+surya-trv-13@users.noreply.github.com>
2021-04-28 09:34:25 +0530

    Class Component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>React KT</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Class Component</h2>
     </div>
   );
 }
