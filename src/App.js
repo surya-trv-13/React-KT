f0775f595ed33ad1dc2d7f55d156a7a89766122c
SURYANARAYAN RATH <45998114+surya-trv-13@users.noreply.github.com>
2021-04-30 10:04:54 +0530

    Redux Init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./styles.css";
 // import { ClassComponent } from "./component/ClassComponent";
-import StateExample from "./component/StateExample";
-import LifeCycleExample from "./component/LifeCycleMethods";
-import Functional from "./component/Functional";
+// import StateExample from "./component/StateExample";
+// import LifeCycleExample from "./component/LifeCycleMethods";
+// import Functional from "./component/Functional";
 
 // Parent Component
 export default function App() {
@@ -10,8 +10,8 @@ export default function App() {
   return (
     <div className="App">
       <h1>React KT</h1>
-      <h2>Functional Component</h2>
-      <Functional data="Prop Value" />
+      <h2>Redux</h2>
+      {/* <Functional data="Prop Value" /> */}
       {/* <StateExample /> */}
       {/* <LifeCycleExample /> */}
       {/* <ClassComponent data="Data 1" data2="Anotehr" />
